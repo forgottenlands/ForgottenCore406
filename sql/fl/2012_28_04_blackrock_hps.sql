20feb2459c909a68b75d57b0a3f2578f7e34220e
dimiandre <dimiandre@gmail.com>
2012-01-28 23:04:56 +0100

    DB/Creature: Blackrock Caverns: Fix HPs of bosses

---
+++ sql/fl/2012_28_04_blackrock_hps.sql
@@ -0,0 +1,19 @@
+-- Rom'ogg healt
+UPDATE `creature_template` SET `Health_mod`=66.24 WHERE `entry`=39665;
+UPDATE `creature_template` SET `Health_mod`=55.0 WHERE `entry`=39666;
+-- Corla health
+UPDATE `creature_template` SET `Health_mod`=68.55 WHERE `entry`=39679;
+UPDATE `creature_template` SET `Health_mod`=65.0 WHERE `entry`=39680;
+UPDATE `creature_template` SET `Health_mod`=22.78 WHERE `entry`=39987;
+-- Karsh
+UPDATE `creature_template` SET `Health_mod`=66.24 WHERE `entry`=39698;
+UPDATE `creature_template` SET `Health_mod`=60.0 WHERE  `entry`=39699;
+-- Beauty
+UPDATE `creature_template` SET `Health_mod`=66.24 WHERE `entry`=39700;
+UPDATE `creature_template` SET `Health_mod`=50 WHERE `entry`=39701;
+UPDATE `creature_template` SET `Health_mod`=22.78 WHERE `entry`=40008;
+UPDATE `creature_template` SET `Health_mod`=22.78 WHERE `entry`=40011;
+UPDATE `creature_template` SET `Health_mod`=22.78 WHERE `entry`=40013;
+-- Lord
+UPDATE `creature_template` SET `Health_mod`=62.08 WHERE `entry`=39705;
+UPDATE `creature_template` SET `Health_mod`=58.8979WHERE `entry`=39706;
