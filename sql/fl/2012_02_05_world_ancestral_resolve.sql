6226a090c7b8b3371cd11aec31adfaa7b7e301a5
dimiandre <dimiandre@gmail.com>
2012-02-05 03:33:09 +0100

    Missing SQL

---
+++ sql/fl/2012_02_05_world_ancestral_resolve.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (77830, 77829);
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(77829, 'spell_sha_ancestral_resolve'),
+(77830, 'spell_sha_ancestral_resolve');
