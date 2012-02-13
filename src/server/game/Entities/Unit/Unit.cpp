ce2a0c391bbcefd34c472ee43c36d500d634315b
dimiandre <dimiandre@gmail.com>
2012-02-13 13:55:20 +0100

    Core/Talens: Death Knight, Fix Dark Simulacrum copy no-mana cost spells

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8647,7 +8647,7 @@ bool Unit::HandleDummyAuraProc(Unit *pVictim, uint32 damage,
                 if (procSpell->EffectBasePoints[0] > 50000 || procSpell->EffectBasePoints[1] > 50000 || procSpell->EffectBasePoints[1] > 50000)
                     return false;
                 
-                if (procSpell->manaCost < 0)
+                if (procSpell->manaCost <= 0)
                     return false;
 
                 int32 bp0 = int32(procSpell->Id);
