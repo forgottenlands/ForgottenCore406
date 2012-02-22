33959b95491d225ab97b48b4e5e2c780b9e5b85e
dimiandre <dimiandre@gmail.com>
2012-02-22 19:27:54 +0100

    Core/DB: Fix Vortex Pinnacle heroic trash HP

---
+++ sql/fl/2012_02_22_vortex_hero_hp.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `Health_mod`=1.8157 WHERE  `entry`=45933;
+UPDATE `creature_template` SET `Health_mod`=10 WHERE  `entry`=45929;
+UPDATE `creature_template` SET `Health_mod`=10 WHERE  `entry`=45927;
