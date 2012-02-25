cb9e78ef43ef8a43efe744879e31370118ba8d6a
dimiandre <dimiandre@gmail.com>
2012-02-25 23:51:28 +0100

    Core/Spells: Priest, fix Divine Touch heal amount

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1172,10 +1172,10 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
             // Renew
             if (GetId() == 139)
             {
-                // Empowered Renew
+                // Divine Touch
                 if (AuraEffect const * aurEff = caster->GetDummyAuraEffect(SPELLFAMILY_PRIEST, 3021, 0))
                 {
-                    int32 basepoints0 = aurEff->GetAmount()	* GetEffect(0)->GetTotalTicks()	* caster->SpellHealingBonus(target, GetSpellProto(), 0, GetEffect(0)->GetAmount(), HEAL) / 100;
+                    int32 basepoints0 = aurEff->GetAmount() * (GetEffect(0)->GetTotalTicks() * caster->SpellHealingBonus(caster, GetSpellProto(), 0, GetEffect(0)->GetAmount()*GetEffect(0)->GetTotalTicks(), HEAL)) / 100;
                     caster->CastCustomSpell(target, 63544, &basepoints0, NULL, NULL, true, NULL, GetEffect(0));
                 }
             }
