ef32613da694bbcfeae94170724bd648c5571d6a
dimiandre <dimiandre@gmail.com>
2012-02-27 12:47:02 +0100

    Fix build

---
+++ sql/fl/2012_02_27_2_spell_script_name.sql
@@ -0,0 +1,6 @@
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(73975, 'spell_dk_necrotic_strike'),
+(50977, 'spell_dk_death_gate'),
+(50371, 'spell_dk_improved_blood_presence'),
+(50391, 'spell_dk_improved_unholy_presence'),
+(50392, 'spell_dk_improved_unholy_presence');
--- src/server/game/Spells/Auras/SpellAuraEffects.cpp
+++ src/server/game/Spells/Auras/SpellAuraEffects.cpp
@@ -1877,11 +1877,7 @@ void AuraEffect::PeriodicTick(AuraApplication * aurApp, Unit * caster) const
                 case 29842: // Second Wind r2
                 case 42771: // Second Wind r2
                     damage = int32(caster->GetMaxHealth() * 0.005f);
-          break;
-         case 8936: // Glifo de Recrecimiento
-                    if (caster->HasAura(54743) && target->HealthBelowPct(50))
-                        this->GetBase()->RefreshDuration();
-                    break;
+                break;
                 case 8936: // Glifo de Recrecimiento
                     if (caster->HasAura(54743) && target->HealthBelowPct(50))
                         this->GetBase()->RefreshDuration();
