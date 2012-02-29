7876d81f8e30a0db161d3dac3b7e50455234c716
dimiandre <dimiandre@gmail.com>
2012-02-29 10:51:01 +0100

    DB/Quests: Fix Daily Heroic rewards

---
+++ sql/fl/custom/2012_02_29_custom_quest_rew.sql
@@ -0,0 +1,4 @@
+UPDATE `quest_template` SET `RewCurrencyCount1`=140, `RewCurrencyCount2`=150 WHERE  `entry`=5000003;
+UPDATE `quest_template` SET `RewCurrencyCount1`=140, `RewCurrencyCount2`=150 WHERE  `entry`=5000002;
+UPDATE `quest_template` SET `RewCurrencyCount1`=140, `RewCurrencyCount2`=150 WHERE  `entry`=5000001;
+UPDATE `quest_template` SET `RewCurrencyCount1`=140, `RewCurrencyCount2`=150 WHERE  `entry`=5000000;
