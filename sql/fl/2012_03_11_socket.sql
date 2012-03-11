2305de5f84d54076efc7789d6766f899108ffe81
dimiandre <dimiandre@gmail.com>
2012-03-11 03:59:48 +0100

    Core/Items: Fix Sockets for certain items

---
+++ sql/fl/2012_03_11_socket.sql
@@ -0,0 +1,6 @@
+UPDATE `item_template` SET `socketColor_1`=2 WHERE `entry`=56563;
+UPDATE `item_template` SET `socketColor_1`=8 WHERE `entry`=55058;
+UPDATE `item_template` SET `socketColor_1`=4 WHERE `entry`=55062;
+UPDATE `item_template` SET `socketColor_1`=2 WHERE `entry`=56564;
+UPDATE `item_template` SET `socketColor_1`=2 WHERE `entry`=56561;
+UPDATE `item_template` SET `socketColor_1`=2 WHERE `entry`=56562;
