e5664018d33656464dc238b1044e9c8a784ecfb4
Airfive <alexsarpato@gmail.com>
2012-03-17 18:54:52 +0100

    DB/Fix: Fix Innkeepers lvl 81-85 Alliance and neutral.

---
+++ sql/fl/2012_03_17_fix_inkeepers_ Ally_Neutral.sql
@@ -0,0 +1,148 @@
+-- Caretaker Nuunwa
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='45300');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='45300');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='45300');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='45300');
+UPDATE `creature_template` SET `faction_A`='35', `faction_H`='35' WHERE (`entry`='45300');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58257');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58261');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58268');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58269');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58275');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('45300', '58280');
+
+-- Ben Mora
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49599');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49599');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49599');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49599');
+
+-- Innkeeper Corlin
+UPDATE `creature_template` SET `minlevel`='85', `maxlevel`='85', `exp`='3', `mindmg`='1007', `maxdmg`='1354.7', `attackpower`='1571', `baseattacktime`='2000' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `minrangedmg`='758.1', `maxrangedmg`='1062.1', `rangedattackpower`='220', `mingold`='15', `maxgold`='33' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `InhabitType`='1' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `WDBVerified`='14480' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49795');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49795');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58257');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58261');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58268');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58269');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58275');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49795', '58280');
+
+-- Naveen Tendernose
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49591');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49591');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49591');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49591');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58257');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58261');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58268');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58269');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58275');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49591', '58280');
+
+-- Vaughn Blusterbeard
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49574');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49574');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49574');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49574');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58257');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58261');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58268');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58269');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58275');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('49574', '58280');
+
+-- Innkeeper Francis
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49688');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49688');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49688');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49688');
+
+-- Innkeeper Teresa
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49686');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49686');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49686');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49686');
+
+-- Kazemde
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='48886');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='48886');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='48886');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='48886');
+
+-- Yasmin
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='49406');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='49406');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='49406');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='49406');
+
+-- Anissa Matherly
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='42873');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='42873');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='42873');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='42873');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42873', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42873', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42873', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42873', '58279');
+
+-- Barracks officer milson
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='42963');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='42963');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='42963');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='42963');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42963', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42963', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42963', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('42963', '58279');
+
+-- Isara Riverstride
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='43487');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='43487');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='43487');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='43487');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43487', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43487', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43487', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43487', '58279');
+
+-- Isara Riverstride FAKE 4.2.0
+DELETE FROM `creature_template` WHERE (`entry`='50068');
+
+-- Salirn Moonbear
+UPDATE `creature_template` SET `unit_flags`='32768' WHERE (`entry`='43378');
+UPDATE `creature_template` SET `dynamicflags`='8' WHERE (`entry`='43378');
+UPDATE `creature_template` SET `npcflag`='66177' WHERE (`entry`='43378');
+UPDATE `creature_template` SET `ScriptName`='npc_innkeeper' WHERE (`entry`='43378');
+
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43378', '58256');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43378', '58260');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43378', '58274');
+INSERT INTO `npc_vendor` (`entry`, `item`) VALUES ('43378', '58279');
+
+-- Salirn moonbear FAKE 4.2.0
+DELETE FROM `creature_template` WHERE (`entry`='53779');
