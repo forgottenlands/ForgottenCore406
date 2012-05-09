eb6807f69039f8bc2decf33b06810560ac41ba6c
Airfive <alexsarpato@gmail.com>
2012-05-09 17:51:38 +0200

    DB/fix: quest 13602

---
+++ sql/fl/2012_05_09_quest_13602.sql
@@ -0,0 +1,6 @@
+-- Quest: Naga of the Strand
+UPDATE `creature_template` SET `KillCredit1`='3711' WHERE (`entry`='3712');
+UPDATE `creature_template` SET `KillCredit1`='3711' WHERE (`entry`='3713');
+UPDATE `creature_template` SET `KillCredit1`='3711' WHERE (`entry`='3715');
+UPDATE `creature_template` SET `KillCredit1`='3711' WHERE (`entry`='3717');
+UPDATE `creature_template` SET `KillCredit1`='3711' WHERE (`entry`='3944');
