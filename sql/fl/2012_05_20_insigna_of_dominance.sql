a7641e3be32068f22c5b0e860a765be97f7b483b
dimiandre <dimiandre@gmail.com>
2012-05-20 23:17:10 +0200

    DB/Procs: Add internal cd to Insigna of Dominance

---
+++ sql/fl/2012_05_20_insigna_of_dominance.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_proc_event` WHERE `entry` = 92219;
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(92219, 0, 0, 0, 0, 0, 348500, 0, 0, 25, 60);
