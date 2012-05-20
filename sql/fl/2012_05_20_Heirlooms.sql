1e5f10f4ac4421e1037bf90322a0f9b965e650e0
Airfive <alexsarpato@gmail.com>
2012-05-21 00:56:04 +0200

    DB/item: removed Heirlooms required rep

---
+++ sql/fl/2012_05_20_Heirlooms.sql
@@ -0,0 +1,13 @@
+-- Heirloom fix
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=62040);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=69887);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=62039);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=61936);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=61931);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=61942);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=69892);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=61937);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=61935);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=61958);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=62038);
+UPDATE `item_template` SET `RequiredReputationFaction`=0, `RequiredReputationRank`=0 WHERE (`entry`=67107);
