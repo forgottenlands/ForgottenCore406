76832d440ff179576ac436eccef56d39e4808ce0
Airfive <alexsarpato@gmail.com>
2012-05-21 15:11:44 +0200

    DB/item: fix Mystic Cogwheel

---
+++ sql/fl/2012_05_21_Mystic_Cogwheel.sql
@@ -0,0 +1,9 @@
+-- Mystic Cogwheel
+UPDATE `item_template` SET `FlagsExtra`=8192 WHERE (`entry`=68660);
+UPDATE `item_template` SET `RequiredSkill`=202, `RequiredSkillRank`=525, `requiredspell`=0 WHERE (`entry`=68660);
+UPDATE `item_template` SET `stat_value1`=0 WHERE (`entry`=68660);
+UPDATE `item_template` SET `stat_type1`=0 WHERE (`entry`=68660);
+UPDATE `item_template` SET `delay`=0 WHERE (`entry`=68660);
+UPDATE `item_template` SET `BagFamily`=128 WHERE (`entry`=68660);
+UPDATE `item_template` SET `GemProperties`=1854 WHERE (`entry`=68660);
+UPDATE `item_template` SET `WDBVerified`=13329 WHERE (`entry`=68660);
