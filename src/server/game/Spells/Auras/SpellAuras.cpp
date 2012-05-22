0c8041c3157f1e7e9049fed6c6e865a268bf2e88
dimiandre <dimiandre@gmail.com>
2012-05-22 22:22:07 +0200

    fix another crash

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -1309,13 +1309,16 @@ void Aura::HandleAuraSpecificMods(AuraApplication const* aurApp, Unit* caster, b
                     }
                     break;
                 // Rejuvenation
-                /*case 774:
+                case 774:
+                    if (!caster)
+                        return;
+
                     if (AuraEffect * aurEff = caster->GetDummyAuraEffect(SPELLFAMILY_DRUID, 3186, 0))
                     {
                         int32 basepoints0 = aurEff->GetAmount() * (GetEffect(0)->GetTotalTicks() * caster->SpellHealingBonus(caster, GetSpellProto(), 0, GetEffect(0)->GetAmount()*GetEffect(0)->GetTotalTicks(), HEAL)) / 100;
                         caster->CastCustomSpell(target, 64801, &basepoints0, NULL, NULL, true, NULL, GetEffect(0));
                     }
-                    break;*/
+                    break;
             }
             break;
         }
