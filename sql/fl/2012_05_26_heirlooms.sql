4c90d41ab933156750126c54535e93c5505aee6f
Airfive <alexsarpato@gmail.com>
2012-05-26 21:36:30 +0200

    DB/item: guild vendor heirloom fix

---
+++ sql/fl/2012_05_26_heirlooms.sql
@@ -0,0 +1,13 @@
+-- fix Heirlooms
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=62040);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=69887);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=62039);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=61936);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=61931);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=61942);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=69892);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=61937);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=61935);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=61958);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=62038);
+UPDATE `item_template` SET `stat_type1`=0, `stat_value1`=0, `stat_type2`=0, `stat_value2`=0, `stat_type3`=0, `stat_value3`=0, `stat_type4`=0, `stat_value4`=0 WHERE (`entry`=67107);
