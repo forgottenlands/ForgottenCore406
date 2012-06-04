c98dde639850c0a6e9830e5a6ecfc611c48e9f39
Airfive <alexsarpato@gmail.com>
2012-06-04 23:48:44 +0200

    DB/item: Fix PVP unique item (vicious/bloodthirsty)

---
+++ sql/fl/2012_06_4_PVP_unique_item.sql
@@ -0,0 +1,28 @@
+-- Vicious unique Ring+Trinket
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60658);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60659);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60645);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60647);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60649);
+
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60794);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60798);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60799);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60800);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60801);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60806);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60806);
+
+-- Bloodthirsty unique Ring+Trinket
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64690);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64691);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64692);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64832);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64833);
+
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64789);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64790);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64791);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64792);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64793);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64794);
