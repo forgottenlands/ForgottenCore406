16f82b9684c0701316a5d152feae68c898403673
Airfive <alexsarpato@gmail.com>
2012-06-05 19:12:17 +0200

    DB/item: pvp ring unique rev2

---
+++ sql/fl/2012_06_5_PVP_unique_item_ 2.sql
@@ -0,0 +1,8 @@
+-- Signet vicious/bloodthirsty and band of bees
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60650);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=60651);
+
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64851);
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=64852);
+
+UPDATE `item_template` SET `Flags`=528384 WHERE (`entry`=58185);
