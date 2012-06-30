2996e62ea9129cecb452d28bc55aa37aa54bb00b
dimiandre <dimiandre@gmail.com>
2012-06-30 22:35:55 +0200

    Missing SQL
    Signed-off-by: dimiandre <dimiandre@gmail.com>

---
+++ sql/fl/2012_06_30_priestess_azil.sql
@@ -0,0 +1,3 @@
+UPDATE `creature_template` SET `ScriptName`='boss_high_priestess_azil' WHERE  `entry`=42333 LIMIT 1;
+UPDATE `creature_template` SET `InhabitType`=5 WHERE  `entry`=42355 LIMIT 1;
+UPDATE `creature_template` SET `ScriptName`='npc_gravity_well' WHERE  `entry`=42499 LIMIT 1;
