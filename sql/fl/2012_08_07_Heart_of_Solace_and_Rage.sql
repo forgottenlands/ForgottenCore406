82b09ff85c3f3edd11d9cf3dfb94b6d294bd6e3e
mik1893 <michele.roscelli@gmail.com>
2012-08-07 22:39:48 +0200

    [DB/ItemProc] Fixed Proc of Heart of Solace and Heart of Rage

---
+++ sql/fl/2012_08_07_Heart_of_Solace_and_Rage.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select * from spell_proc_event where entry = 91361 OR entry = 91817
+LIMIT 0, 1000
+
+-- Date: 2012-08-07 22:25
+*/
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES (91361,0,0,0,0,0,4,0,0,10,90);
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES (91817,0,0,0,0,0,4,0,0,10,90);
+INSERT INTO `spell_proc_event` 
+(`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES (91365,0,0,0,0,0,4,0,0,10,90);
+INSERT INTO `spell_proc_event` 
+(`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES (92346,0,0,0,0,0,4,0,0,10,90);
