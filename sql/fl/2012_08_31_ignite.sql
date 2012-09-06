169510fd1354ee3884a5ff057def12ed09a93678
mik1893 <michele.roscelli@gmail.com>
2012-09-06 14:45:02 +0200

    [Cleanup] Fixed some sql dates

---
+++ sql/fl/2012_08_31_ignite.sql
@@ -0,0 +1,4 @@
+delete from `spell_proc_event` where `entry` = '30482' ;
+
+insert into `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) 
+values('30482','20','3','17','4096','4198400','0','0','0','100','0');
