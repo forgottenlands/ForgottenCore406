157ba5dcd7b92bb80bd0cbf0aa558d71223b4da9
Link <pof1981@hotmail.it>
2012-09-23 10:54:51 +0200

    Druid, Fix prevent cast Faerie Fire and Cyclone under cyclone
    Signed-off-by: dimiandre <dimiandre@gmail.com>

---
+++ sql/fl/2012_09_23_Fix_Spells_Under_Cyclone.sql
@@ -0,0 +1,2 @@
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (33786, -91565, 2, 'Druid - Prevent Faerie Fire under Cyclone');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `type`, `comment`) VALUES (33786, -33786, 2, 'Druid - Prevent Cyclone under Cyclone');
