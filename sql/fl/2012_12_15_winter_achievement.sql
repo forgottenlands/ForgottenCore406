11320521954393cedf0ceb07bf1ff31b2a90fa59
dimiandre <dimiandre@gmail.com>
2012-12-15 00:08:29 +0100

    [Core/Achievements] Winter Veil, fix Scrooge

---
+++ sql/fl/2012_12_15_winter_achievement.sql
@@ -0,0 +1,2 @@
+UPDATE `achievement_criteria_data` SET `value1`=36648 WHERE  `criteria_id`=9838 AND `type`=1 LIMIT 1;
+UPDATE `achievement_criteria_data` SET `value1`=42928 WHERE  `criteria_id`=10442 AND `type`=1 LIMIT 1;
